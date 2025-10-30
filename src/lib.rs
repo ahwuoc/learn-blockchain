2ad3f895d9393e115ca8ed0e1f0fcc2702aca73e
ahwuocdz <ahwuocdz@gmail.com>
2025-10-30 15:50:08 +0700

    feat(block):add Block Struct with Debug + construct

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
 type BlockHash = Vec<u8>;
 
 // Credit: https://stackoverflow.com/a/44378174/2773837
-use std::time::{ SystemTime, UNIX_EPOCH };
+use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn now () -> u128 {
-    let duration = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-    ;
-
+// =====This is function gets the system time=========
+//SystemTime:now -> get system time
+pub fn current_timestamp() -> u128 {
+    let duration = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
     duration.as_secs() as u128 * 1000 + duration.subsec_millis() as u128
 }
-
-pub fn u32_bytes (u: &u32) -> [u8; 4] {
+pub fn u32_bytes(u: &u32) -> [u8; 4] {
     [
         (u >> 8 * 0x0) as u8,
         (u >> 8 * 0x1) as u8,
@@ -21,13 +18,12 @@ pub fn u32_bytes (u: &u32) -> [u8; 4] {
     ]
 }
 
-pub fn u64_bytes (u: &u64) -> [u8; 8] {
+pub fn u64_bytes(u: &u64) -> [u8; 8] {
     [
         (u >> 8 * 0x0) as u8,
         (u >> 8 * 0x1) as u8,
         (u >> 8 * 0x2) as u8,
         (u >> 8 * 0x3) as u8,
-
         (u >> 8 * 0x4) as u8,
         (u >> 8 * 0x5) as u8,
         (u >> 8 * 0x6) as u8,
@@ -35,23 +31,20 @@ pub fn u64_bytes (u: &u64) -> [u8; 8] {
     ]
 }
 
-pub fn u128_bytes (u: &u128) -> [u8; 16] {
+pub fn u128_bytes(u: &u128) -> [u8; 16] {
     [
         (u >> 8 * 0x0) as u8,
         (u >> 8 * 0x1) as u8,
         (u >> 8 * 0x2) as u8,
         (u >> 8 * 0x3) as u8,
-
         (u >> 8 * 0x4) as u8,
         (u >> 8 * 0x5) as u8,
         (u >> 8 * 0x6) as u8,
         (u >> 8 * 0x7) as u8,
-
         (u >> 8 * 0x8) as u8,
         (u >> 8 * 0x9) as u8,
         (u >> 8 * 0xa) as u8,
         (u >> 8 * 0xb) as u8,
-
         (u >> 8 * 0xc) as u8,
         (u >> 8 * 0xd) as u8,
         (u >> 8 * 0xe) as u8,
